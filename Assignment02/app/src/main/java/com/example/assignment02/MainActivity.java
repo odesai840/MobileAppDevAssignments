80fe1859d67c2d42322e1882d496e36daec3e000
odesai840 <odesai840@gmail.com>
2024-08-31 15:53:47 -0400

    assignment 2 ui only

---
+++ Assignment02/app/src/main/java/com/example/assignment02/MainActivity.java
@@ -0,0 +1,29 @@
+// Assignment1B
+// MainActivity.java
+// Ohm Desai and Sullivan Crouse
+
+package com.example.assignment02;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
