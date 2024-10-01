b4412d0933f2e06a2b882a67ac4ba6a94b7eaccf
SullyRC <sullivan.r.crouse@gmail.com>
2024-09-30 21:10:29 -0400

    Finishing MainFragment

--- Assignment08/app/src/main/java/com/example/assignment08/MainActivity.java
+++ Assignment08/app/src/main/java/com/example/assignment08/MainActivity.java
@@ -8,13 +8,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentListener, CreateUserFragment.CreateUserFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main, new MainFragment())
+                .commit();
+    }
+
+    public void gotoCreateUser(){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main, new CreateUserFragment(), "create-user-fragment")
+                .commit();
+    }
+
+    @Override
+    public void gotoProfile(User user) {
+
+    }
+
+    @Override
+    public void gotoCountry() {
+
+    }
+
+    @Override
+    public void gotoDoB() {
 
     }
 }
