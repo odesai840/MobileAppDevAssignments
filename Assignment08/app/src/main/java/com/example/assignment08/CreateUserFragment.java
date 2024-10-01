dc826497d2ba48a62de762aeae513eacfe62640c
odesai840 <odesai840@gmail.com>
2024-09-30 20:28:26 -0400

    assignment 8 starting files

---
+++ Assignment08/app/src/main/java/com/example/assignment08/CreateUserFragment.java
@@ -0,0 +1,147 @@
+// Assignment 08
+// CreateUserFragment.java
+// Ohm Desai and Sullivan Crouse
+
+package com.example.assignment08;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.assignment08.databinding.FragmentCreateUserBinding;
+
+public class CreateUserFragment extends Fragment {
+
+    String DoB;
+    String Country;
+
+    public void setDoB(String doB) {
+        DoB = doB;
+    }
+
+    public void setCountry(String country){
+        Country = country;
+    }
+
+    public CreateUserFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentCreateUserBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentCreateUserBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    CreateUserFragmentListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (CreateUserFragmentListener) context;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (DoB == null){
+            binding.DoBSelected.setText("N/A");
+        } else {
+            binding.DoBSelected.setText(DoB);
+        }
+
+        if (Country == null){
+            binding.countrySelected.setText("N/A");
+        } else {
+            binding.countrySelected.setText(Country);
+        }
+
+        binding.DoBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.gotoDoB();
+            }
+        });
+
+        binding.countryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.gotoCountry();
+            }
+        });
+
+        binding.submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User user = createUserWithInfo();
+                if (user != null){
+                    mListener.gotoProfile(user);
+                }
+            }
+        });
+    }
+
+    private User createUserWithInfo(){
+        String userName = binding.enterName.getText().toString();
+        String userEmail = binding.enterEmail.getText().toString();
+        String userCountry = binding.countrySelected.getText().toString();
+        String userDoB = binding.DoBSelected.getText().toString();
+        String age;
+
+
+        if (userName.isEmpty()) {
+            Toast.makeText(getActivity(), "Name is required", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        if (userEmail.isEmpty()) {
+            Toast.makeText(getActivity(), "Email is required", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        try {
+            Double.valueOf(binding.enterAge.getText().toString());
+            age = binding.enterAge.getText().toString();
+            if (age.isEmpty()){
+                Toast.makeText(getActivity(),"Age is required",Toast.LENGTH_SHORT).show();
+                return null;
+            }
+        } catch (NumberFormatException e){
+            Toast.makeText(getActivity(), "Age must be a number", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        if (userCountry.equals("N/A")) {
+            Toast.makeText(getActivity(),"Country is required",Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        if (userDoB.equals("N/A")) {
+            Toast.makeText(getActivity(),"Date of Birth is required",Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        return new User(userName, userEmail, age, userCountry, userDoB);
+    }
+
+
+
+    public interface CreateUserFragmentListener {
+        void gotoProfile(User user);
+        void gotoCountry();
+        void gotoDoB();
+    }
+}
---
+++ Assignment08/app/src/main/java/com/example/assignment08/SelectDoBFragment.java
@@ -0,0 +1,107 @@
+// Assignment 08
+// SelectDoBFragment.java
+// Ohm Desai and Sullivan Crouse
+
+package com.example.assignment08;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.example.assignment08.databinding.FragmentSelectDoBBinding;
+
+import java.util.Calendar;
+
+public class SelectDoBFragment extends Fragment {
+
+    String selectedDate;
+    Boolean selected = false;
+
+    public SelectDoBFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentSelectDoBBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSelectDoBBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -18);
+
+        binding.calendarView.setMaxDate(calendar.getTimeInMillis());
+
+        long selectedDateInMillis = binding.calendarView.getDate();
+        Calendar selectedCalendar = Calendar.getInstance();
+        selectedCalendar.setTimeInMillis(selectedDateInMillis);
+        int year = selectedCalendar.get(Calendar.YEAR);
+        int month = selectedCalendar.get(Calendar.MONTH);
+        int day = selectedCalendar.get(Calendar.DAY_OF_MONTH);
+        selectedDate = (month + 1) + "/" + day + "/" + year;
+        binding.CalendarTitle.setText(selectedDate);
+
+        binding.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                selected = true;
+                selectedDate = selectedDate = (month + 1) + "/" + day + "/" + year;
+                binding.CalendarTitle.setText(selectedDate);
+            }
+        });
+
+        binding.cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.cancelDoBSelection();
+            }
+        });
+
+        binding.submitButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selected) {
+                    mListener.sendSelectedDoB(selectedDate);
+                } else {
+                    Toast.makeText(getActivity(),"You must select a date!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    DoBSelectionListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (DoBSelectionListener) context;
+    }
+
+    public interface DoBSelectionListener {
+        void cancelDoBSelection();
+        void sendSelectedDoB(String DoB);
+    }
+}
