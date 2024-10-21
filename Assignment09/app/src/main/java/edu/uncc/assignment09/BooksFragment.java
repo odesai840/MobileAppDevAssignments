122b44552b0765afd5af6b11d23cb6f08f830f81
SullyRC <sullivan.r.crouse@gmail.com>
2024-10-20 21:20:49 -0400

    initial commit of Assignment 9

---
+++ Assignment09/app/src/main/java/edu/uncc/assignment09/BooksFragment.java
@@ -0,0 +1,76 @@
+package edu.uncc.assignment09;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import edu.uncc.assignment09.databinding.FragmentBooksBinding;
+
+public class BooksFragment extends Fragment {
+    private static final String ARG_PARAM_GENRE = "ARG_PARAM_GENRE";
+    private String mGenre;
+
+    public static BooksFragment newInstance(String genre) {
+        BooksFragment fragment = new BooksFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM_GENRE, genre);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mGenre = getArguments().getString(ARG_PARAM_GENRE);
+        }
+    }
+
+    ArrayList<Book> mBooks = new ArrayList<>();
+
+    public BooksFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentBooksBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentBooksBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Books");
+        mBooks.clear();
+        mBooks.addAll(Data.getBooksByGenre(mGenre));
+    }
+
+    BooksListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof BooksListener) {
+            mListener = (BooksListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement BooksListener");
+        }
+    }
+
+    interface BooksListener{
+        void closeBooks();
+        void gotoBookDetails(Book book);
+    }
+}
